83c01f3d2d9d7d981fc8a05ef52e67104ee613c0
Bayisa Daba <bayisa615@gmail.com>
2025-04-24 00:56:59 +0300

    add files

--- homepage/login.php
+++ homepage/login.php
@@ -4,10 +4,10 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/footer.css"/>
+		<link rel="icon" href="../images/favicon.png">	
+		<link rel="stylesheet" href="css/login.css"/>	
 		<link rel="stylesheet" href="css/nav.css"/>
-		<link rel="stylesheet" href="css/login.css"/>
-		<link rel="icon" href="../images/favicon.png">
+		<link rel="stylesheet" href="css/footer.css"/>
 
 		<title>TDPSchool</title>
 	</head>
